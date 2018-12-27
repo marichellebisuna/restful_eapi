64a522dfb624777b05c4152b8bcfb4b9dc6b14cd
Marichelle <marichellebisuna@yahoo.com>
2018-12-27 22:49:58 +1100

    Seeded the product and review table with fakers

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name'=>$faker->name,
+        'details'=>$faker->paragraph,
+        'price'=>$faker->numberBetween(100,2000),
+        'stock'=>$faker->numberBetween(0,30),
+        'discount'=>$faker->numberBetween(5,75)
+
     ];
 });
